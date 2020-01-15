b130210c258faa34a43d82ec75c199269b230e16
Luis Barrera <luis.barrera@schibsted.com>
2020-01-15 15:15:32 -0300

    [feat/automated build]: modularize source and destiny data.

--- project-name/app/main.py
+++ project-name/app/main.py
@@ -9,6 +9,33 @@ from utils.query import Query
 from utils.read_params import ReadParams
 from utils.time_execution import TimeExecution
 
+def source_data_pulse(params: ReadParams,
+                      config: getConf):
+    athena = Athena(conf=CONFIG.athenaConf)
+    query = Query()
+    data_athena = athena.get_data(QUERY.query_base_athena(params))
+    athena.close_connection()
+    return data_athena
+
+def source_data_dwh(params: ReadParams,
+                      config: getConf):
+    query = Query()
+    db_source = Database(conf=config.db)
+    data_dwh = db_source.select_to_dict(query \
+                                        .query_base_postgresql(params))
+    db_source.close_connection()
+    return data_past, data_current
+
+def destiny_data(params: ReadParams,
+                 config: getConf,
+                 data_athena: pd.DataFrame,
+                 data_dwh: pd.DataFrame) -> None:
+    query = Query()
+    DB_WRITE = Database(conf=config.db)
+    DB_WRITE.execute_command(query.delete_base(params))
+    DB_WRITE.insert_current(data_athena)
+    DB_WRITE.insert_past(data_dwh)
+    DB_WRITE.close_connection()
 
 if __name__ == '__main__':
     CONFIG = getConf()
@@ -19,15 +46,8 @@ if __name__ == '__main__':
     LOG_FORMAT = DATE_FORMAT + INFO_FORMAT
     logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
     PARAMS = ReadParams(sys.argv)
-    ATHENA = Athena(conf=CONFIG.athenaConf)
-    QUERY = Query()
-    DATA_ATHENA = ATHENA.get_data(QUERY.query_base_athena(PARAMS))
-    ATHENA.close_connection()
-    DB_WRITE = Database(conf=CONFIG.db)
-    DB_WRITE.execute_command(QUERY.delete_base(PARAMS))
-    DATA_PSQL = DB_WRITE.select_to_dict(QUERY.query_base_postgresql(PARAMS))
-    DB_WRITE.insert_data(DATA_ATHENA)
-    DB_WRITE.insert_data(DATA_PSQL)
-    DB_WRITE.close_connection()
+    DATA_ATHENA = source_data_pulse(PARAMS, CONFIG)
+    DATA_DWH = source_data_dwh(PARAMS, CONFIG)
+    destiny_data(PARAMS, CONFIG, DATA_ATHENA, DATA_DWH)
     TIME.get_time()
     LOGGER.info('Process ended successfully.')
