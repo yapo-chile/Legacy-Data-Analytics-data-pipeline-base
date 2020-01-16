22252529bdc19c37e60cf83fdcffb268c210bbb1
Luis Barrera <luis.barrera@schibsted.com>
2020-01-16 17:55:33 -0300

    [fix/permission-rundecksh]: Testing install_dependencies.

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -4,23 +4,24 @@ MODULE_COMPILE=""
 
 function GET_BUILD_MODULE(){
     GIT_LAST_COMMIT=$(git log -p --name-only --oneline | head -1 | awk '{print $1}')
-    GIT_LAST_MERGE=$(git log -p --name-only --oneline | grep "Merge pull request" | head -1 | awk '{print $1}')
+    GIT_LAST_MERGE=$(git log -p --name-only --oneline | grep "Merge pull request #" | head -1 | awk '{print $1}')
     echo "GIT_CURRENT_BRANCH: ${GIT_CURRENT_BRANCH}"
     echo "GIT_LAST_COMMIT: ${GIT_LAST_COMMIT}"
     echo "GIT_LAST_MERGE: ${GIT_LAST_MERGE}"
     if [ "${GIT_LAST_COMMIT}" == "${GIT_LAST_MERGE}" ];
     then
         echo "Compare between Merge pull request"
-        GIT_LAST_MERGE=$(git log -p --name-only --oneline | grep "Merge pull request" | head -2 | tail -1 | awk '{print $1}')
+        GIT_LAST_MERGE=$(git log -p --name-only --oneline | grep "Merge pull request #" | head -2 | tail -1 | awk '{print $1}')
     fi
     echo "GIT_CURRENT_BRANCH: ${GIT_CURRENT_BRANCH}"
     echo "GIT_LAST_COMMIT: ${GIT_LAST_COMMIT}"
     echo "GIT_LAST_MERGE: ${GIT_LAST_MERGE}"
-    git log -p --name-only --oneline | head -80
+    #git log -p --name-only --oneline | head -80
     MODULE_COMPILE=$(git log -p --name-only --oneline ${GIT_LAST_MERGE}..${GIT_LAST_COMMIT} | grep "/" | grep  -v " " | grep -v ".md" | awk '{split($0, val, "/"); print val[1]}' | sort | uniq -c | awk '{print $2}')
 }
 
 function INSTALL_DEPENDENCIES(){
+    echo "${MODULE_COMPILE}" 
     if [ -z "${MODULE_COMPILE}" ];
     then
         echo "No changes detected."
